00c6f67e7d210c147c264b4d36aff9943fde6281
awynx <alwin.puche16@gmail.com>
2026-01-08 00:01:25 +0800

    feat: wrap layout in SafeAreaProvider and GestureHandlerRootView for improved UI handling

--- pest-app/src/app/_layout.tsx
+++ pest-app/src/app/_layout.tsx
@@ -11,6 +11,9 @@ import { useColorScheme } from "@src/hooks/use-color-scheme";
 
 import { GluestackUIProvider } from "@src/components/ui/gluestack-ui-provider";
 import "@src/global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Toaster } from "sonner-native";
 
 export const unstable_settings = {
 	anchor: "(tabs)",
@@ -20,21 +23,30 @@ export default function RootLayout() {
 	const colorScheme = useColorScheme();
 
 	return (
-		<GluestackUIProvider mode="light">
-			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-				<Stack>
-					<Stack.Screen name="(auth)" options={{ headerShown: false }} />
-					<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-					<Stack.Screen
-						name="modal"
-						options={{ presentation: "modal", title: "Modal" }}
-					/>
-					<Stack.Protected guard={false}>
-						<Stack.Screen name="onboarding" options={{ headerShown: false }} />
-					</Stack.Protected>
-				</Stack>
-				<StatusBar style="auto" />
-			</ThemeProvider>
-		</GluestackUIProvider>
+		<SafeAreaProvider>
+			<GestureHandlerRootView>
+				<GluestackUIProvider mode="light">
+					<ThemeProvider
+						value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+						<Stack>
+							<Stack.Screen name="(auth)" options={{ headerShown: false }} />
+							<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+							<Stack.Screen
+								name="modal"
+								options={{ presentation: "modal", title: "Modal" }}
+							/>
+							<Stack.Protected guard={false}>
+								<Stack.Screen
+									name="onboarding"
+									options={{ headerShown: false }}
+								/>
+							</Stack.Protected>
+						</Stack>
+						<StatusBar style="auto" />
+					</ThemeProvider>
+				</GluestackUIProvider>
+				<Toaster />
+			</GestureHandlerRootView>
+		</SafeAreaProvider>
 	);
 }
