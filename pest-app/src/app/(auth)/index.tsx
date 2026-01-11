f50c1a92517d244bfca0bf5c73cf333aa15a3ba0
awynx <alwin.puche16@gmail.com>
2026-01-12 02:16:37 +0800

    feat: implement onboarding check and redirect in Auth component

--- pest-app/src/app/(auth)/index.tsx
+++ pest-app/src/app/(auth)/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AgriNova from "../../components/agri-nova";
+import { useAuthStore } from "@src/store/useAuthStore";
+import { Redirect } from "expo-router";
 
 export default function Auth() {
+	const onboarded = useAuthStore((s) => s.onboarded);
+
+	if (onboarded) return <Redirect href="/(auth)/sign-in" />;
+
 	return (
 		<SafeAreaView className="  flex-1  justify-center items-center p-4 ">
 			<AgriNova centerElements showButton />
