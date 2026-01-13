de03248fabe353d27c3bb14e69ff2912fb828fe6
awynx <alwin.puche16@gmail.com>
2026-01-13 11:32:11 +0800

    feat: add middleware for token verification and user authentication

---
+++ api/src/middlewares/middleware.ts
@@ -0,0 +1,25 @@
+import { verifyToken } from "@src/utils/jsonwebtoken.js";
+import { Request, Response, NextFunction } from "express";
+
+export async function middleware(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const token = req.headers.authorization?.split(" ")[1];
+	if (!token) {
+		res.status(401).json({ message: "Unauthorized" });
+		return;
+	}
+
+	const decoded = verifyToken(token);
+
+	if (!decoded) {
+		res.status(401).json({ message: "Unauthorized" });
+		return;
+	}
+
+	req.body.userId = decoded.id;
+
+	next();
+}
