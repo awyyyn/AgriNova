ab27f45e095e23385d5257c5098f17ca35026630
awynx <alwin.puche16@gmail.com>
2026-01-13 11:40:13 +0800

    feat: add delete user functionality and update user routes

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@src/configs/prisma.js";
+import { checkPassword } from "@src/utils/bcrypt.js";
 import { Request, Response } from "express";
 
 export const updateProfileController = async (req: Request, res: Response) => {
@@ -28,6 +29,46 @@ export const updateProfileController = async (req: Request, res: Response) => {
 		});
 	} catch (error) {
 		console.error(`Error in updateProfileController:`);
+		console.error(error);
+		res.status(500).json({
+			message: "Internal server error!",
+		});
+	}
+};
+
+export const deleteUserController = async (req: Request, res: Response) => {
+	try {
+		const { userId, password } = req.body;
+
+		const user = await prisma.user.findUnique({
+			where: { id: userId },
+		});
+
+		if (!user) {
+			res.status(400).json({ error: true, message: "UnAuthorized!" });
+			return;
+		}
+
+		const isPasswordCorrect = await checkPassword(password, user.password);
+
+		if (!isPasswordCorrect) {
+			res.status(400).json({
+				error: true,
+				message: "Incorrect password!",
+			});
+			return;
+		}
+
+		await prisma.user.delete({
+			where: { id: userId },
+		});
+
+		res.status(200).json({
+			error: false,
+			message: "User deleted successfully",
+		});
+	} catch (error) {
+		console.error(`Error in deleteUserController:`);
 		console.error(error);
 		res.status(500).json({
 			message: "Internal server error!",
--- api/src/routes/user.routes.ts
+++ api/src/routes/user.routes.ts
@@ -1,9 +1,13 @@
-import { changePasswordController } from "@src/controllers/password.controller.js";
+import {
+	deleteUserController,
+	updateProfileController,
+} from "@src/controllers/user.controller.js";
 import { middleware } from "@src/middlewares/middleware.js";
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/edit", middleware, changePasswordController);
+router.post("/edit", middleware, updateProfileController);
+router.delete("/delete", middleware, deleteUserController);
 
 export default router;
