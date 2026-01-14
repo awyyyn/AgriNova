d580607dfc9155ba38d6a5e9f4788adaee5b5d80
awynx <alwin.puche16@gmail.com>
2026-01-14 10:22:34 +0800

    feat: enhance Password component with improved password change handling and user feedback

--- pest-app/src/app/change-password.tsx
+++ pest-app/src/app/change-password.tsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { Formik } from "formik";
-import { changePasswordForm } from "@src/validations";
+import { Formik, FormikHelpers } from "formik";
+import { ChangePasswordForm, changePasswordForm } from "@src/validations";
 import { VStack } from "@src/components/ui/vstack";
 import {
 	FormControl,
@@ -19,19 +19,80 @@ import {
 	InputSlot,
 } from "@src/components/ui/input";
 import { EyeIcon, EyeOffIcon, Key } from "lucide-react-native";
+import { useAuthStore } from "@src/store/useAuthStore";
+import { HStack } from "@src/components/ui/hstack";
+import { formatDate } from "date-fns";
+import { toast } from "sonner-native";
+import { useRouter } from "expo-router";
 
 export default function Password() {
+	const { user, token, setUser } = useAuthStore();
+	const router = useRouter();
 	const [showPassword, setShowPassword] = React.useState({
 		currentPassword: false,
 		password: false,
 		confirmPassword: false,
 	});
 
+	async function handleChangePassword(
+		values: ChangePasswordForm,
+		helpers: FormikHelpers<ChangePasswordForm>
+	) {
+		try {
+			console.log("Changing password...");
+			const link = `${process.env.EXPO_PUBLIC_API_URL}/user/change-password`;
+
+			const response = await fetch(link, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify({
+					currentPassword: values.currentPassword,
+					newPassword: values.password,
+				}),
+			});
+
+			const data = await response.json();
+
+			if (response.status !== 200 || data.error) {
+				throw new Error(data.message || "Failed to change password");
+			}
+			toast.success("Password Updated", {
+				description: "Your password was changed successfully.",
+				duration: 5000,
+				richColors: true,
+			});
+			if (user) {
+				setUser({ ...user, lastChangePassword: new Date().toISOString() });
+			}
+			router.dismissTo("/settings");
+		} catch (error) {
+			toast.error("Something went wrong.", {
+				description: (error as Error).message,
+			});
+		} finally {
+			helpers.setSubmitting(false);
+		}
+	}
+
 	return (
 		<View className="p-[4vw] flex-1 bg-[#ECFDF0]">
 			<Text className="text-lg leading-loose">
-				Choose a strong password and don&apos; reuse it for other accounts
+				Choose a strong password and don&apos;t reuse it for other accounts
 			</Text>
+
+			{user?.lastChangePassword && (
+				<HStack>
+					<Text className="text-sm text-gray-600  ">
+						Last changed password on:{" "}
+					</Text>
+					<Text className="font-semibold">
+						{formatDate(user.lastChangePassword, "MMMM dd, yyyy")}
+					</Text>
+				</HStack>
+			)}
 
 			<Formik
 				initialValues={{
@@ -40,7 +101,7 @@ export default function Password() {
 					confirmPassword: "",
 				}}
 				validationSchema={changePasswordForm}
-				onSubmit={() => {}}>
+				onSubmit={handleChangePassword}>
 				{({
 					handleBlur,
 					handleChange,
@@ -184,11 +245,12 @@ export default function Password() {
 								activeOpacity={0.8}
 								className="rounded-full mt-5 min-w-[200px] disabled:bg-gray-800/20 group py-2 bg-[#52CE19]  "
 								onPress={() => handleSubmit()}
-								disabled={isSubmitting}>
+								// disabled={isSubmitting}
+							>
 								<Text
 									className="disabled:text-gray-800/30 text-white text-center text-xl"
 									disabled={isSubmitting}>
-									Save
+									{isSubmitting ? "Updating..." : "Update Password"}
 								</Text>
 							</TouchableOpacity>
 						</VStack>
