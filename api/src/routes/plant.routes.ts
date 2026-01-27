f6c782b379b2d269595d0cd9f650eef105615dab
awynx <alwin.puche16@gmail.com>
2026-01-27 09:32:47 +0800

    add read plant list endpoint

--- api/src/routes/plant.routes.ts
+++ api/src/routes/plant.routes.ts
@@ -1,9 +1,13 @@
 import { middleware } from "../middlewares/middleware.js";
-import { analyzePlantController } from "../controllers/plant.controller.js";
+import {
+	analyzePlantController,
+	readPlantsController,
+} from "../controllers/plant.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/analyze", middleware, analyzePlantController);
+router.post("/list", middleware, readPlantsController);
 
 export default router;
