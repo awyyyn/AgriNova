6dfef7ec4eeec779fa55ea9700427200f85c730f
awynx <alwin.puche16@gmail.com>
2026-01-27 09:07:38 +0800

    add role in express request type

--- api/src/types/express.d.ts
+++ api/src/types/express.d.ts
@@ -1,7 +1,9 @@
+import { User } from "@src/generated/prisma/client.ts";
 import "express";
 
 declare module "express-serve-static-core" {
 	interface Request {
 		userId?: string;
+		role?: User["role"];
 	}
 }
