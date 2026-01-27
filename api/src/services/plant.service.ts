c0421db7980e269d00ee8c263a92b6721ccaab1a
awynx <alwin.puche16@gmail.com>
2026-01-27 09:07:30 +0800

    create readPlants service

---
+++ api/src/services/plant.service.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@src/configs/prisma.js";
+import { Prisma } from "@src/types/index.js";
+
+interface ReadPlants {
+	pagination?: {
+		page: number;
+		limit: number;
+	};
+	query?: string;
+	userId?: string;
+}
+
+export const readPlantAnalysis = async ({
+	pagination,
+	query,
+	userId,
+}: ReadPlants = {}) => {
+	let where: Prisma.PlantWhereInput = {};
+
+	if (userId) {
+		where.userId = userId;
+	}
+
+	if (query) {
+		where.OR = [
+			{ plantIdentification: { commonName: query } },
+			{ plantIdentification: { scientificName: query } },
+			{ formattedId: { equals: query } },
+			{},
+		];
+	}
+
+	const plants = await prisma.plant.findMany({
+		where,
+
+		skip: pagination ? pagination.limit * pagination?.page : undefined,
+		take: pagination ? pagination.limit : undefined,
+	});
+
+	const total = await prisma.plant.count({ where });
+
+	return {
+		data: plants,
+		hasNextPage: plants.length === pagination?.limit,
+		total,
+	};
+};
