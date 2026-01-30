ef80042e7a694fa2e5c66e43dbeb89f8537b1884
awynx <alwin.puche16@gmail.com>
2026-01-30 13:09:43 +0800

    feat: implement readStatsController for statistical data retrieval

---
+++ api/src/controllers/stats.contnroller.ts
@@ -0,0 +1,49 @@
+import {
+	computeAverageConfidence,
+	computeHealthyPlantsCount,
+	computePlantsWithNoPestsCount,
+	computePlantsWithPestsCount,
+	computeTotalAnalyses,
+	computeUnhealthyPlantsCount,
+	computeWeightedSuccessRate,
+} from "@src/services/stat.service.js";
+import { Request, Response } from "express";
+
+export const readStatsController = async (req: Request, res: Response) => {
+	try {
+		const userId = req.userId;
+
+		const totalAnalyses = await computeTotalAnalyses(userId);
+		const healthyPlantsCount = await computeHealthyPlantsCount(userId);
+		const unhealthyPlantsCount = await computeUnhealthyPlantsCount(userId);
+		const plantsWithNoPestsCount = await computePlantsWithNoPestsCount(userId);
+		const plantsWithPestsCount = await computePlantsWithPestsCount(userId);
+		const averageConfidence = await computeAverageConfidence(userId);
+		const successRate = await computeWeightedSuccessRate(userId);
+
+		console.log({
+			totalAnalyses,
+			healthyPlantsCount,
+			unhealthyPlantsCount,
+			plantsWithNoPestsCount,
+			plantsWithPestsCount,
+			averageConfidence,
+			successRate,
+		});
+		res.status(200).json({
+			totalAnalyses,
+			healthyPlantsCount,
+			unhealthyPlantsCount,
+			plantsWithNoPestsCount,
+			plantsWithPestsCount,
+			averageConfidence,
+			successRate,
+		});
+	} catch (error) {
+		console.error(`Error in readStatsController:`);
+		console.error(error);
+		res.status(500).json({
+			message: "Internal server error!",
+		});
+	}
+};
