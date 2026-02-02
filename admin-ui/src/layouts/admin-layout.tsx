fd0bdbb86bdccf71968b3dfa52f983c8a905529e
awynx <alwin.puche16@gmail.com>
2026-02-02 18:53:41 +0800

    fix: update BreadcrumbLink to use Link component for navigation

--- admin-ui/src/layouts/admin-layout.tsx
+++ admin-ui/src/layouts/admin-layout.tsx
@@ -14,14 +14,17 @@ import {
 	SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { useAuth } from "@/contexts/auth-context";
-import { Navigate, Outlet } from "react-router";
+import { Link, Navigate, Outlet, useLocation } from "react-router";
 
 export default function AdminLayout() {
 	const { isAuthenticated } = useAuth();
+	const paths = useLocation();
 
 	if (!isAuthenticated) {
 		return <Navigate to={"/auth/login"} />;
 	}
+
+	console.log("Current Path:", paths.pathname);
 
 	return (
 		<SidebarProvider>
@@ -37,12 +40,41 @@ export default function AdminLayout() {
 						<Breadcrumb>
 							<BreadcrumbList>
 								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="/admin">AgriNova</BreadcrumbLink>
+									<BreadcrumbLink asChild>
+										<Link to="/admin">AgriNova</Link>
+									</BreadcrumbLink>
 								</BreadcrumbItem>
-								<BreadcrumbSeparator className="hidden md:block" />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-								</BreadcrumbItem>
+								{paths.pathname.split("/").map((segment, index, arr) => {
+									if (segment === "admin" || segment === "") {
+										return null;
+									}
+									const path = `/${arr.slice(1, index + 1).join("/")}`;
+									const isLast = index === arr.length - 1;
+									return (
+										<>
+											<BreadcrumbSeparator
+												key={`${path}-separator`}
+												className="hidden md:block"
+											/>
+											<BreadcrumbItem key={path}>
+												{isLast ? (
+													<BreadcrumbPage>
+														{segment.charAt(0).toUpperCase() + segment.slice(1)}
+													</BreadcrumbPage>
+												) : (
+													<>
+														<BreadcrumbLink asChild>
+															<Link to={path}>
+																{segment.charAt(0).toUpperCase() +
+																	segment.slice(1)}
+															</Link>
+														</BreadcrumbLink>
+													</>
+												)}
+											</BreadcrumbItem>
+										</>
+									);
+								})}
 							</BreadcrumbList>
 						</Breadcrumb>
 					</div>
