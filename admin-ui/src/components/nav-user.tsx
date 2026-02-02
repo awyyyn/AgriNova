4f3e18a81427b251c19c5e78cc8ba5b7ec8a6689
awynx <alwin.puche16@gmail.com>
2026-02-02 18:40:38 +0800

    fix: update avatar image source to use user's photo

--- admin-ui/src/components/nav-user.tsx
+++ admin-ui/src/components/nav-user.tsx
@@ -38,7 +38,7 @@ export function NavUser() {
 							size="lg"
 							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
 							<Avatar className="h-8 w-8 rounded-lg">
-								<AvatarImage src={``} alt={user?.firstName} />
+								<AvatarImage src={user?.photo} alt={user?.firstName} />
 								<AvatarFallback className="rounded-lg">CN</AvatarFallback>
 							</Avatar>
 							<div className="grid flex-1 text-left text-sm leading-tight">
