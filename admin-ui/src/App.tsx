d302e4110242d8e9d8dcc468f8290cca2c3a9d2f
awynx <alwin.puche16@gmail.com>
2026-02-03 17:45:34 +0800

    fix: wrap application in HelmetProvider for improved SEO management

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -2,16 +2,17 @@ import { BrowserRouter } from "react-router";
 import Routes from "./routes";
 import { Toaster } from "sonner";
 import { AuthProvider } from "./contexts/auth-context";
+import { HelmetProvider } from "react-helmet-async";
 
 export default function App() {
 	return (
-		<>
+		<HelmetProvider>
 			<AuthProvider>
 				<BrowserRouter>
 					<Routes />
 				</BrowserRouter>
 			</AuthProvider>
 			<Toaster />
-		</>
+		</HelmetProvider>
 	);
 }
