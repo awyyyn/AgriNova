9f2e530e3bb393bb28b6caef2d3d65e980fb35cd
awynx <alwin.puche16@gmail.com>
2026-02-05 18:17:56 +0800

    add getRangeName util function

--- admin-ui/src/lib/utils.ts
+++ admin-ui/src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const getRangeName = (range: string) => {
+	const rangeNames: Record<string, string> = {
+		today: "Today",
+		yesterday: "Yesterday",
+		last7Days: "Last 7 Days",
+		last30Days: "Last 30 Days",
+		thisWeek: "This Week",
+		lastWeek: "Last Week",
+		thisMonth: "This Month",
+		lastMonth: "Last Month",
+		custom: "Custom Range",
+	};
+	return rangeNames[range] || range;
+};
