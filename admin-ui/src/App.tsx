cd529cf20436937b67ebe95118ddd17ca5f60252
awynx <alwin.puche16@gmail.com>
2026-02-06 15:34:06 +0800

    add tooltip provider

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -3,16 +3,19 @@ import Routes from "./routes";
 import { Toaster } from "sonner";
 import { AuthProvider } from "./contexts/auth-context";
 import { HelmetProvider } from "react-helmet-async";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 export default function App() {
 	return (
 		<HelmetProvider>
-			<AuthProvider>
-				<BrowserRouter>
-					<Routes />
-				</BrowserRouter>
-			</AuthProvider>
-			<Toaster />
+			<TooltipProvider>
+				<AuthProvider>
+					<BrowserRouter>
+						<Routes />
+					</BrowserRouter>
+				</AuthProvider>
+				<Toaster />
+			</TooltipProvider>
 		</HelmetProvider>
 	);
 }
