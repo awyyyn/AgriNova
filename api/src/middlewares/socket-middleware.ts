afecf9becc057b838562720cfcef53bc30d8fd50
awynx <alwin.puche16@gmail.com>
2026-02-11 23:33:41 +0800

    add socket middleware

---
+++ api/src/middlewares/socket-middleware.ts
@@ -0,0 +1,23 @@
+import { verifyToken } from "@src/utils/jsonwebtoken.js";
+import { ExtendedError, Socket } from "socket.io";
+
+export async function SocketIOMiddleware(
+	socket: Socket,
+	next: (err?: ExtendedError | undefined) => void,
+) {
+	const token = socket.handshake.auth.token;
+
+	if (!token) {
+		return next(new Error("Authentication error"));
+	}
+
+	try {
+		const decoded = verifyToken(token);
+
+		socket.data.user = decoded;
+
+		next();
+	} catch (error) {
+		next(new Error("Authentication error"));
+	}
+}
