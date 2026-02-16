6be9cbeb51e48573ef0c5dc0ed8f985e6751b1bd
awynx <alwin.puche16@gmail.com>
2026-02-17 02:18:16 +0800

    change the tab bar icon and text color

--- pest-app/src/app/(tabs)/_layout.tsx
+++ pest-app/src/app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { IconSymbol } from "@src/components/ui/icon-symbol";
 import { Colors } from "@src/constants/theme";
 import { useColorScheme } from "@src/hooks/use-color-scheme";
 import { Aperture, Settings } from "lucide-react-native";
+import { Text } from "react-native";
 
 export default function TabLayout() {
 	const colorScheme = useColorScheme();
@@ -20,23 +21,55 @@ export default function TabLayout() {
 				name="index"
 				options={{
 					title: "Home",
-					tabBarIcon: ({ color }) => (
-						<IconSymbol size={28} name="house.fill" color={color} />
+					tabBarIcon: ({ color, focused }) => (
+						<IconSymbol
+							size={28}
+							name="house.fill"
+							color={focused ? "#2e7d32" : color}
+						/>
 					),
+					tabBarLabel({ focused, color }) {
+						return (
+							<Text
+								className={` ${focused ? "text-[#2e7d32] text-md" : `text-sm ${color}`}`}>
+								Home
+							</Text>
+						);
+					},
 				}}
 			/>
 			<Tabs.Screen
 				name="scan"
 				options={{
 					title: "Scan",
-					tabBarIcon: ({ color }) => <Aperture color={color} />,
+					tabBarIcon: ({ color, focused }) => (
+						<Aperture color={focused ? "#2e7d32" : color} />
+					),
+					tabBarLabel({ focused, color }) {
+						return (
+							<Text
+								className={` ${focused ? "text-[#2e7d32] text-md" : `text-sm ${color}`}`}>
+								Scan
+							</Text>
+						);
+					},
 				}}
 			/>
 			<Tabs.Screen
 				name="settings"
 				options={{
 					title: "Settings",
-					tabBarIcon: ({ color }) => <Settings color={color} />,
+					tabBarIcon: ({ color, focused }) => (
+						<Settings color={focused ? "#2e7d32" : color} />
+					),
+					tabBarLabel({ focused, color }) {
+						return (
+							<Text
+								className={` ${focused ? "text-[#2e7d32] text-md" : `text-sm ${color}`}`}>
+								Settings
+							</Text>
+						);
+					},
 				}}
 			/>
 		</Tabs>
