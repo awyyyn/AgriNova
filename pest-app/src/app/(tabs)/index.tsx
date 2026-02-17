a344109b6afe41558df4c11c5af645ab7a6229fa
awynx <alwin.puche16@gmail.com>
2026-02-17 11:10:35 +0800

    refactor ui with bg image and colors

--- pest-app/src/app/(tabs)/index.tsx
+++ pest-app/src/app/(tabs)/index.tsx
@@ -1,14 +1,30 @@
-import { Text, ActivityIndicator, RefreshControl } from "react-native";
+import {
+	Text,
+	ActivityIndicator,
+	RefreshControl,
+	ScrollView,
+	Dimensions,
+	View,
+} from "react-native";
 import { useEffect, useState, useCallback } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import ScrollViewLayout from "../../layouts/scrollview-layout";
+import { Header } from "@src/components/header";
 import { Card } from "@src/components/ui/card";
 import { Heading } from "@src/components/ui/heading";
 import { Grid, GridItem } from "@src/components/ui/grid";
-import { ChartLine, Scan, Shield } from "lucide-react-native";
+import {
+	Activity,
+	Bug,
+	ChartLine,
+	File,
+	FileText,
+	Scan,
+	Shield,
+} from "lucide-react-native";
 import { Button, ButtonText } from "@src/components/ui/button";
 import { router } from "expo-router";
 import { useAuthStore } from "@src/store/useAuthStore";
+import { ImageBackground } from "expo-image";
 
 type Stats = {
 	totalAnalyses: number;
@@ -63,80 +79,141 @@ export default function HomeScreen() {
 	}, []);
 
 	return (
-		<ScrollViewLayout
+		// <ScrollViewLayout
+		// 	refreshControl={
+		// 		<RefreshControl
+		// 			refreshing={refreshing}
+		// 			onRefresh={onRefresh}
+		// 			tintColor="#41B249"
+		// 		/>
+		// 	}>
+		<ScrollView
 			refreshControl={
 				<RefreshControl
 					refreshing={refreshing}
 					onRefresh={onRefresh}
 					tintColor="#41B249"
 				/>
-			}>
-			<Heading size="4xl">Welcome back</Heading>
-			<Text className="text-xl px-2 mb-2 leading-relaxed tracking-wide">
-				Protect your plants with AI-powered pest detection
-			</Text>
-
-			<Card
-				size="lg"
-				className="rounded-none bg-[#2e7d32] overflow-hidden mb-4">
-				<Heading className="text-white">Quick Scan</Heading>
-				<Text className="text-white text-xl">
-					Detect pest instantly and get treatment recommendations.
+			}
+			bounces={false}
+			alwaysBounceVertical={false}
+			showsVerticalScrollIndicator={false}
+			stickyHeaderIndices={[0]}
+			contentContainerStyle={{ flexGrow: 1 }}>
+			<Header centerElements color="light" />
+			<ImageBackground
+				style={{ flex: 1, padding: Dimensions.get("screen").width * 0.05 }}
+				source={require("../../assets/bg.png")}>
+				<Heading size="4xl" className="text-[#2e7d32]">
+					Welcome back
+				</Heading>
+				<Text className="text-xl px-2 mb-2 leading-relaxed tracking-wide">
+					Protect your plants with AI-powered pest detection
 				</Text>
-				<Button
-					onPress={() => router.push("/modal")}
-					className="bg-white mt-3 rounded-xl max-w-[150px]">
-					<ButtonText className="text-[#2e7d32] text-xl">Start Scan</ButtonText>
-				</Button>
-			</Card>
 
-			<Heading size="2xl" className="mb-2">
-				Your stats
-			</Heading>
+				<Card
+					style={{
+						shadowColor: "#82D55C",
+						shadowOffset: { width: 0, height: 3 },
+						shadowRadius: 4,
+						shadowOpacity: 1,
+						elevation: 10,
+					}}
+					size="lg"
+					className="rounded-2xl border-[#0A980F] border bg-[#E0FFE7] overflow-hidden mb-4">
+					<Heading className="  text-black">Quick Scan</Heading>
+					<Text className="text-black text-xl">
+						Detect pest instantly and get treatment recommendations.
+					</Text>
+					<Button
+						onPress={() => router.push("/modal")}
+						className="ml-auto border-[#0A980F] border bg-white mt-3 rounded-xl max-w-[150px]">
+						<ButtonText className="text-[#2e7d32] text-xl">
+							Start Scan
+						</ButtonText>
+					</Button>
+				</Card>
 
-			{loading && !stats ? (
-				<ActivityIndicator />
-			) : (
-				<Grid className="gap-5" _extra={{ className: "grid-cols-2" }}>
-					<GridItem _extra={{ className: "" }}>
-						<Card size="lg" className="bg-[#2e7d32] rounded-none">
-							<Scan size={32} color="#FFF" />
-							<Text className="mt-4 text-xl text-white font-medium  ">
-								Total Scans
-							</Text>
-							<Text className="text-white text-xl">
-								{stats?.totalAnalyses ?? 0}
-							</Text>
-						</Card>
-					</GridItem>
+				<Heading size="2xl" className="mb-2">
+					Statistics
+				</Heading>
 
-					<GridItem _extra={{ className: "" }}>
-						<Card size="lg" className="bg-[#2e7d32] rounded-none">
-							<ChartLine size={32} color="#FFF" />
-							<Text className="mt-4 text-white font-medium text-xl">
-								Success Rate
-							</Text>
-							<Text className="text-white text-xl">
-								{Number(stats?.successRate ?? 0).toFixed(2)}%
-							</Text>
-						</Card>
-					</GridItem>
+				{loading && !stats ? (
+					<ActivityIndicator />
+				) : (
+					<Grid className="gap-5" _extra={{ className: "grid-cols-1" }}>
+						<GridItem _extra={{ className: "" }}>
+							<Card
+								style={{
+									shadowColor: "#82D55C",
+									shadowOffset: { width: 0, height: 3 },
+									shadowRadius: 4,
+									shadowOpacity: 1,
+									elevation: 10,
+								}}
+								className="  bg-[#F0FDF4] border border-[#2e7d32] rounded-2xl flex flex-row justify-between items-start"
+								size="lg">
+								<View>
+									<Text className=" text-xl text-black font-medium  ">
+										Total Scans
+									</Text>
+									<Text className="text-black text-xl mt-5">
+										{stats?.totalAnalyses ?? 0}
+									</Text>
+								</View>
+								<FileText size={32} color="#2e7d32" />
+							</Card>
+						</GridItem>
 
-					<GridItem _extra={{ className: "" }}>
-						<Card size="lg" className="bg-[#2e7d32] rounded-none">
-							<Shield size={32} color="#FFF" />
-							<Text className="mt-4 text-white font-medium text-xl">
-								Pest Found
-							</Text>
-							<Text className="text-white text-xl">
-								{stats?.plantsWithPestsCount ?? 0}
-							</Text>
-						</Card>
-					</GridItem>
+						<GridItem _extra={{ className: "" }}>
+							<Card
+								style={{
+									shadowColor: "#82D55C",
+									shadowOffset: { width: 0, height: 3 },
+									shadowRadius: 4,
+									shadowOpacity: 1,
+									elevation: 10,
+								}}
+								size="lg"
+								className="bg-[#FFF7ED] border border-[#2e7d32] rounded-2xl flex flex-row justify-between items-start">
+								<View>
+									<Text className="  text-black font-medium text-xl">
+										Success Rate
+									</Text>
+									<Text className="text-black text-xl mt-5">
+										{Number(stats?.successRate ?? 0).toFixed(2)}%
+									</Text>
+								</View>
+								<Activity size={32} color="#F00" />
+							</Card>
+						</GridItem>
 
-					<GridItem _extra={{ className: "" }} />
-				</Grid>
-			)}
-		</ScrollViewLayout>
+						<GridItem _extra={{ className: "" }}>
+							<Card
+								style={{
+									shadowColor: "#82D55C",
+									shadowOffset: { width: 0, height: 3 },
+									shadowRadius: 4,
+									shadowOpacity: 1,
+									elevation: 10,
+								}}
+								size="lg"
+								className="bg-[#FEF2F2] border border-[#2e7d32] rounded-2xl flex flex-row justify-between items-start">
+								<View>
+									<Text className=" text-black font-medium text-xl">
+										Pest Found
+									</Text>
+									<Text className="text-black text-xl mt-5">
+										{stats?.plantsWithPestsCount ?? 0}
+									</Text>
+								</View>
+								<Bug size={32} color="#F00" />
+							</Card>
+						</GridItem>
+					</Grid>
+				)}
+			</ImageBackground>
+		</ScrollView>
+		// </ScrollViewLayout>
 	);
 }
